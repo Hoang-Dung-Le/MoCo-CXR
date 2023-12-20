eae48209eebe1383c6d4b4361b6cdbbdaba30602
Le Hoang Dung <hoangdungle36@gmail.com>
2023-12-20 23:31:52 +0700

    + debug metrics 10

--- moco_pretraining/moco/training_tools/evaluator.py
+++ moco_pretraining/moco/training_tools/evaluator.py
@@ -157,9 +157,11 @@ class Evaluator:
                     # result = metric_func(*args)
 
                     auc_each_class = metric_func(*args)
+                    print(auc_each_class)
                     auc_each_class_array = np.array(auc_each_class)
+                    print(auc_each_class_array)
                     result = np.average(auc_each_class_array[auc_each_class_array != 0])
-                    # print(result)
+                    print(result)
                     
                     metric_meters[metric].update(result, images.size(0))
             
