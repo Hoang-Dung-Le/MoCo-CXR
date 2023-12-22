7cb429749a711781f0e45de1c20971682212cca7
Le Hoang Dung <hoangdungle36@gmail.com>
2023-12-22 23:13:41 +0700

    + test train model

--- moco_pretraining/moco/main_train.py
+++ moco_pretraining/moco/main_train.py
@@ -287,11 +287,11 @@ def main():
     assert len(parameters) == 2  # fc.weight, fc.bias
     
     if args.optimizer == 'sgd':
-        optimizer = torch.optim.SGD(model.parameters(), args.lr,
+        optimizer = torch.optim.SGD(parameters(), args.lr,
                                     momentum=args.momentum,
                                     weight_decay=args.weight_decay)
     elif args.optimizer == 'adam':
-        optimizer = torch.optim.Adam(model.parameters(), args.lr,
+        optimizer = torch.optim.Adam(parameters(), args.lr,
                                      betas=(0.9, 0.999),
                                      weight_decay=args.weight_decay)
         
